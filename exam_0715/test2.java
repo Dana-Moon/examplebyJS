812cce6d970d58c9c165ddd2ba590ed934823d57
reri0312 <reri0312@gmail.com>
2022-07-15 16:21:37 +0900

    문제 6번

---
+++ exam_0715/test2.java
@@ -0,0 +1,14 @@
+package exam_0715;
+
+public class test2 {
+    public static void main(String[] args) {
+        String[] driver = {"문정은", "백소연"};
+
+        say(driver);
+    }
+
+    public static void say(String[] driver) {
+        System.out.println("0번째 : "+driver[0]);
+        System.out.println("1번째 : "+driver[1]);
+    }
+}
